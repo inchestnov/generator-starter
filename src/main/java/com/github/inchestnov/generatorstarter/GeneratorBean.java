5d22b72c6f46bbc161fc48b33130355e48d14062
igorchestnov <chestnov@haulmont.com>
2021-01-05 23:27:41 +0400

    Remove unused application contest from GeneratorBean

--- src/main/java/com/github/inchestnov/generatorstarter/GeneratorBean.java
+++ src/main/java/com/github/inchestnov/generatorstarter/GeneratorBean.java
@@ -1,18 +1,11 @@
 package com.github.inchestnov.generatorstarter;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import static com.github.inchestnov.generatorstarter.utils.Preconditions.checkNotNull;
 
 @Component
 public class GeneratorBean implements Generator {
-
-    private final ApplicationContext applicationContext;
-
-    public GeneratorBean(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
-    }
 
     @Override
     public <T> T generate(Class<T> instanceClass) {
